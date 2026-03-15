3cd3bb6a9b05a9842d570983d8b80a0ed8453de1
Harry Whorlow <whorlowharry@gmail.com>
2026-03-15 12:49:23 +0100

    chore: bump versions

--- packages/hotkeys-devtools/src/components/index.tsx
+++ packages/hotkeys-devtools/src/components/index.tsx
@@ -1,11 +1,14 @@
+import { ThemeContextProvider } from '@tanstack/devtools-ui'
 import { HotkeysContextProvider } from '../HotkeysContextProvider'
 import { Shell } from './Shell'
 
-import { ThemeContextProvider } from '@tanstack/devtools-ui'
+import type { TanStackDevtoolsTheme } from '@tanstack/devtools-ui'
 
-import type { DevtoolProps } from '@tanstack/devtools-utils/solid'
+interface DevtoolsProps {
+  theme: TanStackDevtoolsTheme
+}
 
-export default function HotkeysDevtools(props: DevtoolProps) {
+export default function HotkeysDevtools(props: DevtoolsProps) {
   return (
     <ThemeContextProvider theme={props.theme}>
       <HotkeysContextProvider>
